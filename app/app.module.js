f568b3a96fff233ac4d7b2cbeeaf931ce88ce84b
yoobin <yoobin@ttadag.com>
2015-11-24 16:33:25 +0900

    [master -> 수정] 최초 진입할대 클릭이벤트 걸어봄, 각페이지별 template파일 업데이트...

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,8 @@
-app.controller('appModuleController',['$scope', function($scope, $http, $routeParams, $location) {
+app.controller('appModuleController',['$scope','$location',function($scope, $location) {
+	$scope.appClickEvent = function() {
+		alert('이동해요...');
+		$location.url('/account/signIn');
+	};
 
 }]);
 
