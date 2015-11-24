70d539607badd1fe5a2de00a0adaad4c616f1089
yoobin <yoobin@ttadag.com>
2015-11-24 21:56:16 +0900

    [master -> 개선] css 성공실패 아이콘 이미지변경 및 적용, 이메일 체크 메서드 적용

--- app/src/account/signIn/signInController.js
+++ app/src/account/signIn/signInController.js
@@ -1,3 +1,12 @@
-app.controller('signInController',['$scope', function($scope, $http, $routeParams, $location) {
-	console.log('signInController');
+app.controller('signInController',['$scope', function($scope) {
+	$scope.resetPasswd = function() {
+		alert('신규 비밀번호를 적용 할 수 있도록 메일을 보냈습니다.');
+	};
+
+}])
+.controller('yoobinController',['$scope', function($scope) {
+	$scope.email = {
+		text : ''
+	};
+
 }]);
