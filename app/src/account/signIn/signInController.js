d06cf9628aa09219040713c681fe14d721772e70
yoobin <yoobin@ttadag.com>
2015-11-25 15:49:34 +0900

    [master -> 개선] 사용자 정보가 없는 경우 회원가입페이지로 땜빵..;

--- app/src/account/signIn/signInController.js
+++ app/src/account/signIn/signInController.js
@@ -3,22 +3,27 @@ app.controller('signInController',['$scope', '$location', function($scope, $loca
 		alert('신규 비밀번호를 적용 할 수 있도록 메일을 보냈습니다.');
 	};
 	$scope.signIn = function(user) {
-		console.log(user);
-		alert('login try...');
-		$location.url('/account/signUp');
-	}
+
+		if(!user) {
+			alert('login try...');
+			$location.url('/account/signUp');
+		}
+
+	};
+
 	$scope.FBSignIn = function(user) {
 
 		alert('페이스북 로그인~ 일단 회원가입으로 보냄..');
 		//user.user_email
 		//user.user_password
 		$location.url('/account/signUp');
-	}
+	};
+
 	$scope.accountCheck = function(info) {
 		if (!!info) {
 			$location.url('/account/signUp');
 		}
-	}
+	};
 
 }])
 .controller('signInFormController',['$scope', function($scope) {
