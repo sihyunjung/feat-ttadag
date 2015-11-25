c697c6638e9a44ec1148ef28d26a8e4c26d71e59
yoobin <yoobin@ttadag.com>
2015-11-25 11:55:09 +0900

    [master -> 개선] 입력 폼체크 기능 개선

--- app/src/account/signIn/signInController.js
+++ app/src/account/signIn/signInController.js
@@ -2,20 +2,16 @@ app.controller('signInController',['$scope', function($scope) {
 	$scope.resetPasswd = function() {
 		alert('신규 비밀번호를 적용 할 수 있도록 메일을 보냈습니다.');
 	};
-	$scope.signIn = function() {
-		alert('로그인 되었어요~~');
+	$scope.signIn = function(user) {
+		console.log(user);
+		alert('login try...');
 	}
 	$scope.FBSignIn = function() {
 		alert('페이스북 로그인~');
 	}
 
 }])
-.controller('yoobinController',['$scope', function($scope) {
-	$scope.email = {
-		text : ''
-	};
-	$scope.password= {
-		text : ''
-	};
+.controller('signInFormController',['$scope', function($scope) {
+
 
 }]);
