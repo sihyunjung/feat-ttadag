4d0941e07d76f4fc54be9b47106446385f8d6cd2
yoobin <yoobin@ttadag.com>
2015-12-11 18:08:57 +0900

    보어 컴포넌트 파일 이름 리네임

--- public/app.module.js
+++ public/app.module.js
@@ -1,8 +1,4 @@
 angular.module('app', [])
 .controller('appModuleController', ['$scope',function ($scope) {
-//	console.log('appCont');
-//	//console.log(account);
-//	//console.log(container);
-//	//account.signIn();
-//	//account.signUp();
+
 }]);
--- public/app.router.js
+++ public/app.router.js
@@ -5,16 +5,4 @@ app.config(function($routeProvider) {
 			controller : 'appModuleController',
 			templateUrl : '/src/introLoading/introLoadingView.html'
 		});
-		//.when('/account', {
-		//	controller : 'accountController',
-		//	templateUrl : '/src/account/accountView.html'
-		//})
-		//.when('/account/signIn', {
-		//	controller : 'signInController',
-		//	templateUrl : '/src/account/signIn/signInView.html'
-		//})
-		//.when('/account/signUp', {
-		//	controller : 'signUpController',
-		//	templateUrl : '/src/account/signUp/signUpView.html'
-		//});
 });
